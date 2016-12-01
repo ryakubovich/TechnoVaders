595cff901115440a3a825442ac2b2ee7362de431
Roman Yakubovich <r.yakubovich14@gmail.com>
2016-12-02 02:54:10 +0300

    Added settings saving and loading

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -3,7 +3,6 @@
 #include <QApplication>
 #include <QWidget>
 #include <QPushButton>
-#include <QComboBox>
 #include <QFormLayout>
 #include <iostream>
 
@@ -12,6 +11,7 @@ typedef void (QWidget::*QWidgetVoidSlot)();
 MainWindow::MainWindow()
 {
   // TO DO: more styling
+  TSettings loadedSettings = LoadSettings();
   setFixedSize(600, 600);
   QPushButton * launchButton = new QPushButton("Launch", m_mainWidget);
   QPushButton * settingsButton = new QPushButton("Settings", m_mainWidget);
@@ -21,18 +21,20 @@ MainWindow::MainWindow()
   exitButton->move(250, 340);
   connect(launchButton, SIGNAL(clicked()), this, SLOT(OnMainLaunchClicked()));
   connect(settingsButton, SIGNAL(clicked()), this, SLOT(OnMainSettingsClicked()));
-  connect(exitButton, SIGNAL(clicked()), this, SLOT(close()));
+  connect(exitButton, SIGNAL(clicked()), this, SLOT(OnMainExitClicked()));
 
   QPushButton * backButton = new QPushButton("Back", m_settingsWidget);
   backButton->move(0, 0);
   connect(backButton, SIGNAL(clicked()), this, SLOT(OnSettingsBackClicked()));
   // TO DO: add control settings
   QComboBox * difficultyChoice = new QComboBox(m_settingsWidget);
+  difficultyChoice->setObjectName("difficulty");
   difficultyChoice->addItems({ "Low", "Medium", "High" });
-  difficultyChoice->setCurrentIndex(0);
+  difficultyChoice->setCurrentText(QString(loadedSettings.find("Difficulty")->second.c_str()));
   QComboBox * resolutionChoice = new QComboBox(m_settingsWidget);
+  resolutionChoice->setObjectName("resolution");
   resolutionChoice->addItems({ "640x480", "800x600", "1024x768", "Full screen"});
-  resolutionChoice->setCurrentIndex(1);
+  resolutionChoice->setCurrentText(QString(loadedSettings.find("Resolution")->second.c_str()));
   QFormLayout * layout = new QFormLayout(m_settingsWidget);
   layout->addRow("Resolution", resolutionChoice);
   layout->addRow("Difficulty", difficultyChoice);
--- src/main_window.hpp
+++ src/main_window.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <QListWidget>
+#include "logger.hpp"
+#include "json/json.h"
+#include "json/value.h"
+#include <fstream>
+#include <map>
+#include <string>
+#include <QComboBox>
 #include <QMainWindow>
 #include <QTimer>
 #include <QGridLayout>
 #include <QOpenGLWidget>
 #include <QStackedWidget>
+
+using TSettings = std::map<std::string, std::string>;
 
 class MainWindow : public QMainWindow
 {
@@ -20,8 +30,45 @@ private:
   QWidget * m_settingsWidget = new QWidget();
   QStackedWidget * m_stack = new QStackedWidget();
 
+  TSettings LoadSettings() const
+  {
+    Json::Value settings;
+    TSettings result;
+    std::ifstream file("data/settings.json");
+    if (!file.is_open()) return result;
+    file >> settings;
+    file.close();
+    Json::Value & common = settings["Common"];
+    result["Resolution"] = common["Resolution"].asCString();
+    result["Difficulty"] = common["Difficulty"].asCString();
+    // TO DO: add control settings load
+    return result;
+  }
+
+  bool WriteSettings()
+  {
+    Json::Value settings;
+    std::ofstream file("data/settings.json");
+    if (file.is_open())
+    {
+      auto & common = settings["Common"];
+      common["Resolution"] = Json::Value(m_settingsWidget->findChild<QComboBox *>("resolution")->currentText().toStdString());
+      common["Difficulty"] = Json::Value(m_settingsWidget->findChild<QComboBox *>("difficulty")->currentText().toStdString());
+      Json::StyledWriter styledWriter;
+      file << styledWriter.write(settings);
+      file.close();
+      return true;
+    }
+    return false;
+  }
+
 private slots:
   void OnMainSettingsClicked() { m_stack->setCurrentIndex(1); }
   void OnMainLaunchClicked() { if (m_glWidget != nullptr) setCentralWidget(m_glWidget); }
   void OnSettingsBackClicked() { m_stack->setCurrentIndex(0); }
+  void OnMainExitClicked()
+  {
+    if (!WriteSettings()) Logger::Instance() << "Unsuccessful write of the settings file";
+    close();
+  }
 };
